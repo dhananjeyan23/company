04bf298e00a01b1cbdac5ce0889b58f09334fb78
dhananjeyan23 <jeyan.mech7@gmail.com>
2024-07-28 09:36:41 +0530

    opp

--- src/org/demo/emp3.java
+++ src/org/demo/emp3.java
@@ -4,14 +4,19 @@ import org.emp3.company;
 
 public class emp3 {
 	String ename3="sdj";
+	String ename4="sdjppp";
 	//create the 1st method empName
 	private void empName2() {
 	System.out.println("employeename : "+ename3);
 	}
+	private void empName4() {
+		System.out.println("employeename : "+ename4);
+		}
   //create the main method 
 	public static void main(String[] args) {
 	//classname objrefname = new classname()
 		emp3 ei1 = new emp3();
 		ei1.empName2();
+		ei1.empName4();
 }
 }
